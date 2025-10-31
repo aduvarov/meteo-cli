6ae1478582cc9447cf890f7cb98a746f5aa2b5f2
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-31 08:50:21 +0500

    ADD: Работа с файловой системой

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,8 +1,37 @@
 import { homedir } from 'os'
 import { join } from 'path'
+import { writeFile, readFile, stat } from 'fs/promises'
 
-const saveKeyValue = (key, value) => {
-    console.log(join(homedir(), '.config', 'meteo-cli.json'))
+const config = join(homedir(), '.config', 'meteo-cli.json')
+
+const saveKeyValue = async (key, value) => {
+    let data = {}
+    const mode = 0o600
+
+    if (await isExist(config)) {
+        const file = await readFile(config)
+        data = JSON.parse(file)
+    }
+
+    data[key] = value
+    await writeFile(config, JSON.stringify(data), { mode })
 }
 
-export { saveKeyValue }
+const getKeyValue = async key => {
+    if (await isExist(config)) {
+        const file = await readFile(config)
+        const data = JSON.parse(file)
+        return data[key]
+    }
+}
+
+const isExist = async file => {
+    try {
+        await stat(file)
+        return true
+    } catch (error) {
+        return false
+    }
+}
+
+export { saveKeyValue, getKeyValue }
